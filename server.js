1f892867ac87d19e9a389c11752ef4ba491ed0b1
waleed khan <waleedkhansafimd983@gmail.com>
2026-03-26 12:48:10 -0700

    server file updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 await connectDB();
 const app = createApp();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`)
 })
